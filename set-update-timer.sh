f8db638561625b3613ff13b934b3da0c95a15fa5
sf44tdw <sf44tdw@users.noreply.github.com>
2026-01-25 08:21:39 +0900

    rootユーザののプロファイルで実行するように変更。

--- set-update-timer.sh
+++ set-update-timer.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+readonly ROOT_HOME='/root'
+readonly ROOT_BASH_PROFILE_PATH="${ROOT_HOME}/.bash_profile"
+readonly ROOT_PROFILE_PATH="${ROOT_HOME}/.profile"
+
+if [ -f "${ROOT_BASH_PROFILE_PATH}" ]; then
+	source "${ROOT_BASH_PROFILE_PATH}" >/dev/null 2>&1 || exit 200
+elif [ -f "${ROOT_PROFILE_PATH}" ]; then
+	source "${ROOT_PROFILE_PATH}" >/dev/null 2>&1 || exit 210
+else
+	exit 220
+fi
+
 ls -l flatpak-update.*
 echo
 chmod 700 flatpak-update.*
@@ -16,6 +28,3 @@ systemctl enable flatpak-update.timer || exit 3
 systemctl start flatpak-update.timer || exit 4
 systemctl status flatpak-update.timer || exit 5
 systemctl list-timers | grep 'flatpal-update' || exit 6
-
-
-
